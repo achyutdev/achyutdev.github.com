a74ee4320580a7cb335666297de6f8cd0cb30f35
Achyut Devkota <achyut.dev@gmail.com>
2015-08-28 22:16:59 +0545

    Blog is added - clean blog theme

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){       
    var scroll_start = 0;
-   var startchange = $('#contact');
-   var offset = startchange.offset();
-    if (startchange.length){
+   var contact_section = $('#contact');
+   var contact = contact_section.offset();
+   var portfolio_section =$('#portfolio');
+   var portfolio=portfolio_section.offset();  
+   console.log(contact);
+   console.log(portfolio);
+  if (contact_section.length){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top-50) {
-          $(".navbar").css('background-color', '#77A836');
-       } else {
+      if(scroll_start < contact.top-50 && scroll_start > portfolio.top-50) {
+          $(".navbar").css('background-color', '#1D83C1');
+       } 
+      else if(scroll_start > contact.top-50){
+        $(".navbar").css('background-color', '#77A836');
+      }
+      else {
           $('.navbar').css('background-color', 'transparent');
        }
    });
